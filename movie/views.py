5b8bd723574dd441cf1f71740c7cb590a9e8c5a2
juanvilla23 <camilovilla20167@gmail.com>
2024-07-31 18:18:29 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -6,6 +6,6 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1> Hola soy Juan Camilo </h1>')
     #return render(request,'home.html')
-    return render(request,'home.html',{'name':'Greg Lim'})
+    return render(request,'home.html',{'name':' Juan Camilo '})
 #def about(request):
     #return HttpResponse('<h1>Welcome to about page</h1>')
