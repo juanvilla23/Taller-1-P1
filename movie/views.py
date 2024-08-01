348d6325af26b61bd36ca6ef2d0325027bfdd18c
juanvilla23 <camilovilla20167@gmail.com>
2024-07-31 22:54:36 -0500

    busquedad de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
 
 # Create your views here.
 def home(request):
-    #return HttpResponse('<h1> Hola soy Juan Camilo </h1>')
-    #return render(request,'home.html')
-    return render(request,'home.html',{'name':' Juan Camilo '})
-def about(request):
-    #return HttpResponse('<h2>Welcome to ABOUT page</h2>')
-    return render(request,'about.html')
+    #return HttpResponse('<h1>Welcome to Home Page</h1>')
+    #return render(request, 'home.html')
+    #return render(request, 'home.html', {'name':'Juan Camilo'})
 
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
+
+def about(request):
+    return HttpResponse('<h1>Welcome to About Page</h1>')
--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from movie import views as MovieViews
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -25,3 +27,4 @@ urlpatterns = [
     path('', MovieViews.home, name='home'), 
     path('about',MovieViews.about,name='about'),
 ]
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
